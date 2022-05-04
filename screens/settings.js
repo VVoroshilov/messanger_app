60018873f8313148be97b9d18c71df24e62684bc
Vladislav <vvsvoroshilov@gmail.com>
2022-05-04 23:50:05 +0300

    Added setting of user picture:

--- screens/settings.js
+++ screens/settings.js
@@ -8,6 +8,8 @@ import SQLite from 'react-native-sqlite-storage';
 import ChatListItem from "../components/chat_list_item";
 import { Avatar } from 'react-native-paper';
 import Icons from 'react-native-vector-icons/MaterialIcons';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
 const defaultPicture = "iVBORw0KGgoAAAANSUhEUgAAAgAAAAIAAgMAAACJFjxpAAAADFBMVEXFxcX////p6enW1tbAmiBwAAAFiElEQVR4AezAgQAAAACAoP2pF6kAAAAAAAAAAAAAAIDbu2MkvY0jiuMWWQoUmI50BB+BgRTpCAz4G6C8CJDrC3AEXGKPoMTlYA/gAJfwETawI8cuBs5Nk2KtvfiLW+gLfK9m+r3X82G653+JP/zjF8afP1S//y+An4/i51//AsB4aH+/QPD6EQAY/zwZwN8BAP50bh786KP4+VT+3fs4/noigEc+jnHeJrzxX+NWMDDh4g8+EXcnLcC9T8U5S/CdT8bcUeBEIrwBOiI8ki7Ba5+NrePgWUy89/nYyxQ8Iw3f+pWY4h1gb3eAW7sDTPEOsLc7wK1TIeDuDB+I/OA1QOUHv/dFsZQkhKkh4QlEfOULYz2nGj2/Nn1LmwR/86VxlCoAW6kCsHRGANx1RgCMo5Qh2EsZgrXNQZZShp5Liv7Il8eIc5C91EHY2hxk6bwYmNscZIReDBwtCdhbErC1JGBpScBcOgFMLQsZMQs5Whayd+UQsLYsZGlZyNyykKllISNmIUfAwifw8NXvTojAjGFrdYi11SGWVoeYWx1i6lmQCiEjFkKOVgjZ+xxIhZCtFULWHkCqxCw9gNQKmP9vNHzipdEPrRcxtVbAeDkAvve0iM2QozVD9hfjhp4YP/UrkJYDbD2AtBxgfSkAvvHEeNcDSAsilgtAWxIy91J8AXgZAJ5e33+4tuACcAG4AFwALgBXRXQB6AFcB5MXAuA6nl9/0Vx/011/1V5/1/dfTPJvRtdnu/zL6beeFO/7r+fXBYbrEkt/j+i6ytXfpuvvE/ZXOnsA/a3a/l5xf7O6v1t+Xe/vOyz6HpO8yyboM8o7rfJes77bru83THk48p7TvOs27zvOO6/73vO++z7l4cgnMPQzKPopHC0N9noSSz6LJp/Gk88jyicy5TOp6qlc+VyyfDJbPpuuns6XzyfMJzTmMyrrKZ35nNJ8Ums+q7af1tvPK+4nNodEnPKp3fnc8npyez67/qVP7+/fL8hfcMjfsOhf8cjfMclfcnn9+BkOnLECP8Q58OYeyJ40eoyF6Ee/En/JHlP6mIlRVXprF4BxtAvArV0AxtEuALd2ARhHuwDc2gVgHPX/hFv9fMBddjIGeKg/WCxlCsI46u+Ga5mCcJd+sIG9UkGAW32ZbApFAHhod4Bb3eo04h3god0BbiUHYApVCNjbHeBW+QDAXT4a7qg7r7e214057vg0QhkEHkoSwq0kIdydXw4/Q3H8hjYJ3vL0WConBJhCHQaOToeBrU0BljYFmEoVgHGUKgAPnREAt84IgLuqFgAYSUEOAHszDwuAtSkHAZhLGYIpdCLgKGUIHtocZG1zkLmUIRhxDnJU1RDA1uYga5uDzKUOwhTnIEfnxcDe5iBrcyQAYGlzkKkUYhhxDrKXQgxbSwLWUohhbknA1JKAEZOAvSUBW0sC1pYEzC0JmFoSMMJyCDhaFrK3JGDtyiFgaVnI3LKQqWUhI2YhR8tC9paFrC0LWVoWMrcsZGpZyIhZyNGykL2rSIGtlQHWVgZYWhlgbmWAqZUBRiwDHK0MsLcywNbKAGsOoNUhllaHmFsdYmp1iBHrEEerQ+w5gFYI2VodYm11iKXVIeYcQCuETK0QMmIh5MgBtELI3gohWyuErDmAVolZWiFkzgG0SszUKjGjfj6gVmKOVonZcwCtFbB9HQC+ozWDbz1bvGu9iKW1AuYcQOtFTLEX1GbIaFegN0OOHEBrhuw5gNYM2XIArRuz5gDacoB3bTnAEktxXQ4wfw0AvveM8b4tiJjSJOwLIsbXsAKeNeKCiOO3D+AVbUl0AfjGs8ZPbUnIdgFoa1LWC0BblfMuB9AeC1j6gqQE0J9LmC8AOYD2ZMb7i4bt2ZTpWoHfPoB7Tj2fXzT8N1X41vkq/QHOAAAAAElFTkSuQmCC"
 
 export default class App extends React.Component {
@@ -28,6 +30,7 @@ export default class App extends React.Component {
     this.getUser();
     this.get_user_data = this.get_user_data.bind(this);
     this.logout = this.logout.bind(this);
+    this.openGallery = this.openGallery.bind(this);
   }  
 
   // DB methods
@@ -57,6 +60,8 @@ export default class App extends React.Component {
             )
         })
 }
+
+
   // Main screen method (request to API for login and inserting server response to the local DB)
   async get_user_data(){
         console.log("fetching user data");
@@ -103,6 +108,29 @@ export default class App extends React.Component {
         )
     }
 
+
+    async openGallery(){
+        const options = {
+            mediaType: "photo",
+            includeBase64: true,
+            };
+        
+        await launchImageLibrary(options, async response => {
+        console.log('Response = ', response);
+        if (response.didCancel) {
+        console.log(`User cancelled image picker`);
+        } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        } else {
+            const base64_string = await response.assets[0].base64;
+            const server_response = await api.load_user_pic(this.state.user_id, this.state.token, base64_string);
+            this.get_user_data()
+        }
+    });
+        };
+
     componentDidMount(){
         this.getUser();
       this.willFocusSubscription = this.props.navigation.addListener(
@@ -133,6 +161,13 @@ export default class App extends React.Component {
                 <SafeAreaView style={styles.container}>
                     <View style={{alignItems:"center"}}>
                         <Avatar.Image size={175} source={{uri: `data:image/jpeg;base64,${this.state.picture}`}} style={{marginTop:20, backgroundColor: "#FFFFFF"}}/>
+                        
+                        <TouchableOpacity style={styles.logout_btn} onPress={this.openGallery}>
+                            <Text style={[styles.logout_text,{color: "#4f9bec"}]}>
+                                Set new image
+                            </Text>
+                        </TouchableOpacity>
+
                         <View style={styles.userinfo}>
                             {this.renderRow([
                             {title: 'Username', value: this.state.username},
@@ -140,6 +175,7 @@ export default class App extends React.Component {
                             {title: 'Bio', value: this.state.bio},
                             ])}
                         </View>
+
                         <TouchableOpacity style={styles.logout_btn} onPress={this.logout}>
                             <Text style={styles.logout_text}>
                                 Logout
